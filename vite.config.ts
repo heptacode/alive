7ba3e33f6e705c52079b0f258168190b88509614
heptacode <admin@hyunwoo.kim>
2022-04-23 16:50:34 +0900

    Remove PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import viteComponents, { VuetifyResolver } from 'vite-plugin-components';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -17,6 +16,5 @@ export default defineConfig({
     viteComponents({
       customComponentResolvers: [VuetifyResolver()],
     }),
-    VitePWA({}),
   ],
 });
