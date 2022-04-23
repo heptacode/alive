1110c3ecd0339f72c0fd728bbd8eb2f558a72691
heptacode <admin@hyunwoo.kim>
2022-04-23 16:49:47 +0900

    Update

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,12 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import x5GMaps from 'x5-gmaps';
-import { registerSW } from 'virtual:pwa-register';
 
 Vue.config.productionTip = false;
 
 Vue.use(x5GMaps, {
   key: 'AIzaSyBBGdySgTvDkEKMZaM5udB_rGP4jct-w60',
   libraries: ['visualization'],
-});
-
-const updateSW = registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
 });
 
 new Vue({
