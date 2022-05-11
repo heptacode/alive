8c5d7c66f8d5e3b1c1c38d6f436a17072f9e776a
heptacode <admin@hyunwoo.kim>
2022-05-11 14:01:48 +0900

    Fix PWA Build Issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: 'workbox-window',
+    },
+  },
   define: {
     'process.env': process.env,
   },
